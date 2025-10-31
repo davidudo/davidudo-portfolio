f38bcc242c494f06c58558441139c1259bcb79aa
davidudo <udodavid46.ud@gmail.com>
2025-10-31 17:31:55 +0100

    chore: add custom fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="font-primary">Welcome to SvelteKit</h1>
+<p class="font-secondary">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
