312e8431285e1267b66d4a85184137bbe0ad2ef8
David Udo <65337929+davidudo@users.noreply.github.com>
2025-11-20 19:17:17 +0100

    Close menu when nav link is clicked

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,29 @@
 // Menu Bar
-const toggle = document.querySelector(".toggle")
-const menu = document.querySelector(".nav-menu")
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".nav-menu");
 
 function toggleMenu() {
   if (menu.classList.contains("active")) {
-    menu.classList.remove("active")
+    menu.classList.remove("active");
 
     // add menu icon
-    toggle.innerHTML = `<i class="fas fa-bars">`
-  }
-  else {
-    menu.classList.add("active")
+    toggle.innerHTML = `<i class="fas fa-bars">`;
+  } else {
+    menu.classList.add("active");
 
     // add x (close menu) icon
-    toggle.innerHTML = `<i class="fas fa-times"></i>`
+    toggle.innerHTML = `<i class="fas fa-times"></i>`;
   }
 }
 
-toggle.addEventListener("click", toggleMenu, false)
+toggle.addEventListener("click", toggleMenu, false);
+
+// Close mobile menu when a nav link is clicked
+const navLinks = document.querySelectorAll(".nav-menu .nav-item a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+    toggle.innerHTML = `<i class="fas fa-bars"></i>`;
+  });
+});
